15b5a8a9dcbe763db8cef348c41d067b820810e0
Jamie Guneratnam <84152510+jamieprince@users.noreply.github.com>
2022-01-04 14:39:46 +0000

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,5 @@
-//Get the button:
+//Back to top button (codes from online resources):
 mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,18 +9,4 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-function showLoader()
-{
-    $(".loader").fadeIn("slow");
-}
-function hideLoader()
-{
-    $(".loader").fadeOut("slow");
-}
+\
